1fb7d1f29424b3b3265aeac4fe2379a3ddae6e76
Gustavo Costa <gustavocosta2407@gmail.com>
2025-11-22 19:28:52 -0300

    Update urls.py
    adiciona rotas de envio e histórico de mensagens no chat

--- backend/chat/urls.py
+++ backend/chat/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path("send/", views.send_message),
-    path("history/<str:user>/", views.history),
+    path("history//", views.history),
 ]
